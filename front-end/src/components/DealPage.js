9b636403494681e737832fffb13eb06ffdc7b1f0
Thomas Hanna <thomasjhanna@gmail.com>
2018-11-29 01:26:42 +0000

    Styling deals page

--- front-end/src/components/DealPage.js
+++ front-end/src/components/DealPage.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import ContentContainer from '../styled-components/ContentContainer';
+import { Button } from '../styled-components/Button';
 
 class DealPage extends React.Component {
   constructor(props) {
@@ -57,6 +58,11 @@ class DealPage extends React.Component {
       deal_expired: dealExpired
     } = this.state;
 
+    const fixedPrice = Math.round(price * 100) / 100;
+    const fixedNextBestPrice = Math.round(nextBestPrice * 100) / 100;
+
+    console.log(fixedNextBestPrice);
+
     const facebookShareUri = `https://www.facebook.com/share.php?u=${encodeURIComponent(
       dealLink
     )}&title=${encodeURIComponent(dealTitle)}`;
@@ -65,44 +71,88 @@ class DealPage extends React.Component {
     )}&text=${encodeURIComponent(dealTitle)}&via=deals-ie`;
 
     return (
-      <ContentContainer>
-        <a href={dealLink} target="_blank" rel="noopener noreferrer">
-          <DealPageImage src={imageUrl} />
-        </a>
-        <DealPageHeatContainer>
-          {'-'}
-          {dealLikes - dealDislikes}
-          {'+'}
-        </DealPageHeatContainer>
-        <DealPageTitleContainer>{dealTitle}</DealPageTitleContainer>
-        <DealPagePriceContainer>
-          <DealPagePrice>{price}</DealPagePrice>
-          <DealPageNextBestPrice>{nextBestPrice}</DealPageNextBestPrice>
-          <DealPageUsername>{userName}</DealPageUsername>
+      <DealPageContainer>
+        <DealPageDealContainer>
           <a href={dealLink} target="_blank" rel="noopener noreferrer">
-            <DealPageDealButton>Go to deal</DealPageDealButton>
+            <DealPageImage src={imageUrl} />
           </a>
-          <DealPageTextContainer>
-            <DealPageText>{dealText}</DealPageText>
-          </DealPageTextContainer>
-          <a href={facebookShareUri} target="_blank" rel="noopener noreferrer">
-            <DealPageFacebookPostButton>Post</DealPageFacebookPostButton>
-          </a>
-          <a href={twitterShareUri} target="_blank" rel="noopener noreferrer">
-            <DealPageTwitterPostButton>Tweet</DealPageTwitterPostButton>
-          </a>
-        </DealPagePriceContainer>
-      </ContentContainer>
+          <DealPageDetailsContainer>
+            <DealPageHeatContainer>
+              {'-'}
+              {dealLikes - dealDislikes}
+              {'+'}
+            </DealPageHeatContainer>
+            <DealPageTitleContainer>{dealTitle}</DealPageTitleContainer>
+            <DealPagePriceContainer>
+              <DealPagePrice>{fixedPrice}</DealPagePrice>
+              <DealPageNextBestPrice>{fixedNextBestPrice}</DealPageNextBestPrice>
+            </DealPagePriceContainer>
+            <DealPageUserAndDealButtonContainer>
+              <DealPageUsername>{userName}</DealPageUsername>
+              <a href={dealLink} target="_blank" rel="noopener noreferrer">
+                <DealPageDealButton>Go to deal</DealPageDealButton>
+              </a>
+            </DealPageUserAndDealButtonContainer>
+          </DealPageDetailsContainer>
+        </DealPageDealContainer>
+        <DealPageTextContainer>
+          <DealPageText>{dealText}</DealPageText>
+        </DealPageTextContainer>
+        <a href={facebookShareUri} target="_blank" rel="noopener noreferrer">
+          <DealPageFacebookPostButton>Post</DealPageFacebookPostButton>
+        </a>
+        <a href={twitterShareUri} target="_blank" rel="noopener noreferrer">
+          <DealPageTwitterPostButton>Tweet</DealPageTwitterPostButton>
+        </a>
+
+        <DealPageCommentsContainer>
+          <div>
+            <p>Placeholder comment</p>
+          </div>
+        </DealPageCommentsContainer>
+      </DealPageContainer>
     );
   }
 }
 
-const DealPageImage = styled.img`
-  width: 200px;
-  height: 200px;
+const DealPageContainer = styled.div`
+  font-weight: 400;
+  margin: 1rem auto;
+  padding: 2rem; /* m-size */
+  max-width: 135rem;
+  background-color: var(--light-grey);
+  border-radius: 5px;
 `;
 
-const DealPageHeatContainer = styled.div``;
+const DealPageDealContainer = styled.div`
+  display: flex;
+`;
+
+const DealPageImage = styled.img`
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  box-shadow: 5px;
+
+  transition: all 0.2s;
+
+  &:hover {
+    transform: scale(1.03);
+    box-shadow: 0 8px 6px -6px black;
+  }
+`;
+
+const DealPageDetailsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 200px;
+  padding: 1rem;
+`;
+
+const DealPageHeatContainer = styled.div`
+  display: inline-block;
+`;
 
 const DealPageTitleContainer = styled.div``;
 
@@ -112,17 +162,27 @@ const DealPagePrice = styled.span``;
 
 const DealPageNextBestPrice = styled.span``;
 
+const DealPageUserAndDealButtonContainer = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const DealPageUsername = styled.span``;
 
-const DealPageDealButton = styled.button``;
+const DealPageDealButton = styled(Button)``;
 
 const DealPageTextContainer = styled.div``;
 
-const DealPageText = styled.p``;
+const DealPageText = styled.p`
+  white-space: pre-line;
+`;
 
 const DealPageFacebookPostButton = styled.button``;
 
 const DealPageTwitterPostButton = styled.button``;
+
+const DealPageCommentsContainer = styled.div``;
 
 DealPage.propTypes = {
   location: PropTypes.shape({
