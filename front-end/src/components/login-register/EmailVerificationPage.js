6c8822d3c2df6556f01b101cb81ff896d4925fe8
Thomas Hanna <thomasjhanna@gmail.com>
2018-12-11 16:20:23 +0000

    Prior to moving login and register modal to AppRouter

--- front-end/src/components/login-register/EmailVerificationPage.js
+++ front-end/src/components/login-register/EmailVerificationPage.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Formik, Form } from 'formik';
-import * as yup from 'yup';
-import format from 'date-fns/format';
+// import { Formik, Form } from 'formik';
+// import * as yup from 'yup';
+// import format from 'date-fns/format';
 
-import {
-  TextField,
-  TextareaField,
-  NumericField,
-  CheckboxField,
-  StyledErrorMessage,
-  ErrorSpan
-} from '../../styled-components/FormikStyles';
-import {
-  RadioContainer,
-  RadioGroup,
-  RadioInput,
-  RadioLabel,
-  RadioButton
-} from '../../styled-components/Radio';
 import Label from '../../styled-components/Label';
 import { ContentContainerPasswordForm } from '../../styled-components/ContentContainer';
 import { Button } from '../../styled-components/Button';
@@ -35,12 +20,11 @@ class EmailVerificationPage extends React.Component {
     this.state = {
       displayVerified: false
     };
-    this.onSubmitEmail = this.onSubmitEmail.bind(this);
+    // this.onSubmitEmail = this.onSubmitEmail.bind(this);
   }
 
   componentDidMount() {
-    console.log(this.props);
-    const { location } = this.props;
+    const { location, history } = this.props;
 
     const index = location.search.indexOf('=');
 
@@ -66,88 +50,55 @@ class EmailVerificationPage extends React.Component {
       .catch(console.log);
   }
 
-  onSubmitEmail(values) {
-    // const token = window.sessionStorage.getItem('token');
-
-    fetch(`${window.BACKEND_PATH}/deals`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(values)
-    })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        if (data && data.deal_title) {
-          // loadUser(user);
-          history.push(`/deals/${data.id}`); // .push is not a function?
-        }
-      })
-      .catch(console.log);
-  }
-
   render() {
+    console.log(this.state);
+    const { displayVerified } = this.state;
     return (
-      <ContentContainerPasswordForm>
-        <CheckEmailContainer>
-          <h2>Please check your email</h2>
-          <SmallList>
-            <li>
-              <SmallListText> Click on the verify link provided in that email</SmallListText>
-            </li>
-            <li>
+      <div>
+        {displayVerified ? (
+          <ContentContainerPasswordForm>
+            <CheckEmailContainer>
+              <h2>Please check your email</h2>
+              <SmallList>
+                <li>
+                  <SmallListText> Click on the verify link provided in that email</SmallListText>
+                </li>
+                <li>
+                  <SmallListText>
+                    {' '}
+                    If you did not recieve an email please make sure to check your spam folder
+                  </SmallListText>
+                </li>
+                <li>
+                  <SmallListText>
+                    If there is still no sign of the email, you can request another using the form
+                    below
+                  </SmallListText>
+                </li>
+              </SmallList>
+            </CheckEmailContainer>
+            <CheckEmailContainer>
+              <h2>Email Verified Success!</h2>
+              <SmallListText>Thank you for verifying your email address.</SmallListText>
+              <SmallListText>Click below to login.</SmallListText>
+              <Button type="submit">Login page</Button>
+            </CheckEmailContainer>
+          </ContentContainerPasswordForm>
+        ) : (
+          <ContentContainerPasswordForm>
+            <CheckEmailContainer>
+              <h2>Verify email failed..</h2>
+              <SmallListText>The token provided is not valid or has expired.</SmallListText>
               <SmallListText>
-                {' '}
-                If you did not recieve an email please make sure to check your spam folder
+                Click the button below to request another verification email.
               </SmallListText>
-            </li>
-            <li>
-              <SmallListText>
-                If there is still no sign of the email, you can request another using the form below
-              </SmallListText>
-            </li>
-          </SmallList>
-        </CheckEmailContainer>
-        <h2>Request another email</h2>
-        <Formik
-          initialValues={{
-            email: ''
-          }}
-          validationSchema={yup.object().shape({
-            email: yup
-              .string()
-              .email('E-mail is not valid!')
-              .required('E-mail is required!')
-          })}
-          onSubmit={(values, { setSubmitting }) => {
-            this.onSubmitEmail(values);
-            setSubmitting(false);
-          }}
-        >
-          {({ errors, touched, isSubmitting }) => {
-            console.log(errors);
-            console.log(touched);
-            return (
-              <Form>
-                <Label htmlFor="email">
-                  Email
-                  <StyledErrorMessage name="email" component="span" />
-                  <TextField
-                    autoComplete="off"
-                    type="text"
-                    name="email"
-                    placeholder="Please enter your email address"
-                  />
-                </Label>
-                <Button type="submit" disabled={isSubmitting}>
-                  Submit
-                </Button>
-              </Form>
-            );
-          }}
-        </Formik>
-      </ContentContainerPasswordForm>
+              <Link to="/complete-signup-request">
+                <Button type="submit">Request another email</Button>
+              </Link>
+            </CheckEmailContainer>
+          </ContentContainerPasswordForm>
+        )}
+      </div>
     );
   }
 }
@@ -163,6 +114,7 @@ const SmallList = styled.ul`
 
 const SmallListText = styled.p`
   font-size: 1.3rem;
+  margin-bottom: 1rem;
 `;
 
 export default withRouter(EmailVerificationPage);
