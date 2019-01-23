eb2f14105da6ebd30cd157ccb39b42daf75e808c
Thomas Hanna <thomasjhanna@gmail.com>
2019-01-23 03:57:49 +0000

    Added compression gzip package

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path'); // require is a built in node function
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -73,6 +74,9 @@ module.exports = env => {
         'process.env.mode': JSON.stringify(process.env.mode),
         'process.env.NGINX_PRODUCTION': JSON.stringify(process.env.NGINX_PRODUCTION)
       })
+      // new CompressionPlugin({
+      //   filename: '[path].gz[query]'
+      // })
       // new BundleAnalyzerPlugin()
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
